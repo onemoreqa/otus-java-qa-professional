406a50c77f73298d94d644b6a5c9dc9d893a4128
Egor Gorbunov <yigorbu4@mts.ru>
2024-03-19 03:07:54 +0500

    add sql test

---
+++ src/test/java/helpers/SqlGetRequest.java
@@ -0,0 +1,40 @@
+package helpers;
+
+import com.consol.citrus.TestActionRunner;
+import com.consol.citrus.TestBehavior;
+import com.consol.citrus.actions.ExecuteSQLAction;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.context.TestContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.sql.DataSource;
+
+import static com.consol.citrus.actions.ExecuteSQLQueryAction.Builder.query;
+import static com.consol.citrus.http.actions.HttpActionBuilder.http;
+
+public class SqlGetRequest implements TestBehavior {
+
+    private TestContext context;
+    public String pathUri;
+
+    public SqlGetRequest(String pathUri, TestContext context) {
+        this.context = context;
+        this.pathUri = pathUri;
+    }
+
+    @Autowired
+    public DataSource sqlHelper;
+
+    /***
+     * По аналогии не заработал @TODO надо бы добить ApplyBehaviorSql
+     **/
+    @Override
+    @CitrusTest
+    public void apply(TestActionRunner testActionRunner) {
+        testActionRunner.run(
+                query(sqlHelper)
+                        .statement("SELECT * FROM public.users where username = 'Oleg';")
+        );
+    }
+
+}
