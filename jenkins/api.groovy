0d2b22da7be7d01c79b55d3382793a1e840685df
Egor Gorbunov <yigorbu4@mts.ru>
2024-04-12 02:32:54 +0500

    debig

--- jenkins/api.groovy
+++ jenkins/api.groovy
@@ -8,6 +8,14 @@ branch: $BRANCH
         """
         }
 
+        config = readYaml text: env.YAML_CONFIG
+
+        if (config != null) {
+            for(param in config.entrySet()) {
+                env.setProperty(param.getKey(), param.getValue())
+            }
+        }
+
         stage("Api tests in docker image") {
             sh "docker run -v /root/.m2/repository:/root/.m2/repository -v ./surefire-reports:/home/ubuntu/api_tests/target/surefire-reports -v ./allure-results:/home/ubuntu/api_tests/target/allure-results localhost:5005/apitests:${env.getProperty('TEST_VERSION')}"
             //sh "sleep 300"
