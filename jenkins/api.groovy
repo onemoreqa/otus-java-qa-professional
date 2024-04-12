c940fcb47341d99d1559fa3b1fc0456a53751146
Egor Gorbunov <yigorbu4@mts.ru>
2024-04-12 15:56:11 +0500

    add params

--- jenkins/api.groovy
+++ jenkins/api.groovy
@@ -17,7 +17,7 @@ branch: $BRANCH
         }
 
         stage("API tests in docker image") {
-            sh "docker run -v /root/.m2/repository:/root/.m2/repository -v ./surefire-reports:/home/ubuntu/api_tests/target/surefire-reports -v ./allure-results:/home/ubuntu/api_tests/target/allure-results localhost:5005/apitests:0.0.1 ${env.getProperty('PARALLEL')}"
+            sh "docker run -v /root/.m2/repository:/root/.m2/repository -v ./surefire-reports:/home/ubuntu/api_tests/target/surefire-reports -v ./allure-results:/home/ubuntu/api_tests/target/allure-results localhost:5005/apitests:${env.getProperty('TEST_VERSION')} ${env.getProperty('PARALLEL')}"
             //sh "sleep 300"
         }
 
--- jenkins/ui.groovy
+++ jenkins/ui.groovy
@@ -17,7 +17,7 @@ branch: $BRANCH
         }
 
         stage("UI tests in docker image") {
-            sh "docker run -v /root/.m2/repository:/root/.m2/repository -v ./surefire-reports:/home/ubuntu/ui_tests/target/surefire-reports -v ./allure-results:/home/ubuntu/ui_tests/target/allure-results localhost:5005/uitests:0.0.1 1 chrome http://95.181.151.41/wd/hub 120.0"
+            sh "docker run -v /root/.m2/repository:/root/.m2/repository -v ./surefire-reports:/home/ubuntu/ui_tests/target/surefire-reports -v ./allure-results:/home/ubuntu/ui_tests/target/allure-results localhost:5005/uitests:${env.getProperty('TEST_VERSION')} ${env.getProperty('PARALLEL')} ${env.getProperty('BROWSER_NAME')} ${env.getProperty('SELENOID_URL')} ${env.getProperty('BROWSER_VERSION')}"
             //sh "sleep 300"
         }
 
