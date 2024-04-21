18ae1cbc1dbaf29490f49b1d5874a1efeadf1f73
Egor Gorbunov <yigorbu4@mts.ru>
2024-04-22 00:53:55 +0500

    pass arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
 
-if [[ -z "$1" ]]; then
-  command="mvn test"
-else
-  command="mvn test -Dparallel=$1"
-fi
+parallel=4
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+
+  case $key in
+    --parallel)
+    parallel="$2"
+    shift # past argument
+    ;;
+    --help)
+    echo "Usage: $0 [--parallel <parallel>]"
+    echo "  --parallel <parallel>   Set junit parallel thread count (e.g. 2)"
+    echo "  --help               Show this help message"
+    exit 0
+    ;;
+    *)
+    # unknown option
+    ;;
+  esac
+  shift # past argument or value
+done
+
+# Run mvn test with specified keys
+command="mvn test -Dparallel=$parallel"
 
 echo "Run: " $command
 eval $command
